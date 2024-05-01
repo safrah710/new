dd63aa28c1b5a41cd4afcfcbe586acc031163275
safrah710 <158799961+safrah710@users.noreply.github.com>
2024-05-02 00:04:20 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -5,48 +5,12 @@ let n1;
 let result=0;
 let result1=1;
 function modify(event) {
-      if (!(event.target.value === "+" || event.target.value === "-" || event.target.value === "*" || event.target.value === "/")) {
-        n += event.target.value;
-        input.value += event.target.value;
-    } else if ((event.target.value === "+" || event.target.value === "-" || event.target.value === "*" || event.target.value === "/")) {
-        input.value = " ";
-        let symbol = event.target.value;
-        let n1 = Number(n);
-        if (symbol === "+") {
-            result = result + n1;
-        }
-        if (symbol === "-") {
-            if (result === 0) {
-                result = n1-result;
-            } else {
-                result = result-n1;
-            }
-        }
-        if (symbol === "*") {
-            if (result === 0) {
-                result = result1 * n1;
-            } else {
-                result = result * n1;
-            }
-        }
-        if (symbol === "/") {
-            if (result === 0) {
-                result = n1 /result1;
-            } else {
-                result = result / n1;
-            }
-        }
-
-        n = ''; // Reset n for the next number input
-    }
+     input.value+=event.target.value;
 }
 function res()
 {
-    input.value=result;
+    input.value=eval(input.value)
 }
 function cl(){
     input.value="";
-    n = ''; 
-    result = 0; 
-    result1 = 1;
 }
